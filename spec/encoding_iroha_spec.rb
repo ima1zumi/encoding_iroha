8bd59996baba6e72c62ba06554c911d9081bc782
ima1zumi <mariimaizumi5@gmail.com>
2021-09-04 16:53:40 +0900

    Add spec

--- spec/encoding_iroha_spec.rb
+++ spec/encoding_iroha_spec.rb
@@ -1,11 +1,23 @@
-# frozen_string_literal: true
-
 RSpec.describe EncodingIroha do
-  it "has a version number" do
-    expect(EncodingIroha::VERSION).not_to be nil
+  it 'can be encoded with Encoding::IROHA' do
+    bytes = 'いろは'.encode(Encoding::IROHA).bytes
+    expected = [128, 129, 130]
+    expect(bytes).to eq expected
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'cannot be converted should raise an exception' do
+    expect { 'α'.encode(Encoding::IROHA) }.to raise_error(Encoding::UndefinedConversionError)
+  end
+
+  it 'should be valid encoding' do
+    expect("\x80\x81\x82".force_encoding(Encoding::IROHA).valid_encoding?).to be_truthy
+  end
+
+  it 'can find Encoding::IROHA' do
+    expect(Encoding.find('IROHA')).to be Encoding::IROHA
+  end
+
+  it 'should not dummy encoding' do
+    expect(Encoding::IROHA.dummy?).to be_falsey
   end
 end
